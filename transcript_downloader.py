1771490842b827dddc03dca0a6402243a809a8b0
ajanthaselvam <ajanthaselvam@gmail.com>
2024-09-09 20:52:42 +0530

    Update transcript_downloader.py

--- transcript_downloader.py
+++ transcript_downloader.py
@@ -1,18 +1,48 @@
 from youtube_transcript_api import YouTubeTranscriptApi as yta
+from pytube import YouTube
+import webbrowser
 
-link = 'https://youtu.be/VI3f-GaRoIM?si=Ena18bvIK1y-0-j5'
-id = link.split('/')
-vid_id = id[-1]
+def get_youtube_details():
+    # Input the YouTube video URL
+    video_url = input("Enter the YouTube video URL: ")
+    # Create a YouTube object
+    yt = YouTube(video_url)
 
-data = yta.get_transcript(vid_id)
-print(data)
+    # Get the video title and thumbnail URL
+    video_title = yt.title
+    thumbnail_url = yt.thumbnail_url
 
-final_data = ''
-for val in data:
-    for key,value in val.items():
-        if key == 'text':
-            final_data += value
-process_data = final_data.splitlines()
-clean_data = ''.join(process_data)
 
-#print(clean_data)
+    # Open the video in a web browser
+    webbrowser.open(video_url)
+
+# Display the video details
+    print("Playing YouTube video:")
+    print("Title:", video_title)
+    print("Thumbnail URL:", thumbnail_url)
+
+
+def get_youtube_timeframe():
+    link = 'https://youtu.be/VI3f-GaRoIM?si=Ena18bvIK1y-0-j5?t=50s'
+    id = link.split('/')
+    vid_id = id[-1]
+
+    data = yta.get_transcript(vid_id)
+    print(data)
+
+    final_data = ''
+    for val in data:
+        for key,value in val.items():
+            if key == 'text':
+                final_data += value
+    process_data = final_data.splitlines()
+    clean_data = ''.join(process_data)
+
+    #print(clean_data)
+
+def main():
+    get_youtube_details()
+    get_youtube_timeframe()
+
+if __name__ == "__main__":
+    main()
